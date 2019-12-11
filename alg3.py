40772ed58eaeff80fd0a8c0ac06e7c441b4c644e
yehiyam <yehiyam@users.noreply.github.com>
2019-12-11 16:07:04 +0200

    Update alg3.py

--- alg3.py
+++ alg3.py
@@ -1,7 +1,7 @@
 import numpy as np
 def start(args, hkubeApi=None):
     print('start called')
-    waiter1 = hkubeApi.start_algorithm('eval-alg', [5, 6], resultAsRaw=True)
+    waiter1 = hkubeApi.start_algorithm('eval-alg', [5, 6, 7], resultAsRaw=True)
     waiter2 = hkubeApi.start_stored_subpipeline('simple', {'d': [6, 'stam']})
     res = [waiter1.get(), waiter2.get()]
     print('got all results')
